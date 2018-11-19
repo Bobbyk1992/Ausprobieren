ce3022bd80c22ed03b52d9a7aac48f8ab0794543
Bobbyk1992 <Kaibobby1992@gmail.com>
2018-11-19 17:45:28 +0100

    Zweiter Commit auf mein Repos

---
+++ testfile.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.route('/init')
+def homepage():
+        user = 'Harald'
+        title = 'Home'
+        return render_template('init.html', user=user, title=title)
